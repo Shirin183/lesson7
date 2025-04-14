3f42e338db095fd66d2ffa9cc1102eb52ce7efb3
Shirin183 <rumayarumiya@gmail.com>
2025-04-14 22:11:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ for (let i = 1; i <= 10; i++) {
+     console.log('Привет! Номер: ' + i);
+ }
+
+
+ let i = 1;
+ while (i <= 5) {
+     console.log('Цикл while: ' + i);
+     i++;
+ }
+
+ for (let i = 2; i <= 20; i++) {
+     if (i%2==0){
+         console.log('Привет! Номер: ' + i);
+     }
+ }
